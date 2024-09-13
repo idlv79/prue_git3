8da34e40c1aa14cc32913a7fd48cbd640c5751ed
DanielLeivaV <ignacioleiva79@gmail.com>
2024-09-13 16:10:52 -0400

    2 comit

---
+++ N1.php
@@ -0,0 +1,15 @@
+<?php
+echo 'hola';
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    mas de lo mismo
+</body>
+</html>
